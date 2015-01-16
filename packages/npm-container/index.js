b2b1f52b71a9f6926cce784d31c5e4e14ffb85a8
dr.dimitru <dr.dimitru@gmail.com>
2015-01-16 08:36:40 +0300

    Updated and working app

--- packages/npm-container/index.js
+++ packages/npm-container/index.js
@@ -1,9 +1,9 @@
-  Meteor.npmRequire = function(moduleName) {                                             // 79
-    var module = Npm.require(moduleName);                                                // 80
-    return module;                                                                       // 81
-  };                                                                                     // 82
-                                                                                         // 83
-  Meteor.require = function(moduleName) {                                                // 84
-    console.warn('Meteor.require is deprecated. Please use Meteor.npmRequire instead!'); // 85
-    return Meteor.npmRequire(moduleName);                                                // 86
-  };                                                                                     // 87
+  Meteor.npmRequire = function(moduleName) {                                             // 85
+    var module = Npm.require(moduleName);                                                // 86
+    return module;                                                                       // 87
+  };                                                                                     // 88
+                                                                                         // 89
+  Meteor.require = function(moduleName) {                                                // 90
+    console.warn('Meteor.require is deprecated. Please use Meteor.npmRequire instead!'); // 91
+    return Meteor.npmRequire(moduleName);                                                // 92
+  };                                                                                     // 93
